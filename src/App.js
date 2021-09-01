62036deeec2833151d4b18b06bc3acd7e5d6a1d5
DivyatejaChe <54347537+DivyatejaChe@users.noreply.github.com>
2021-09-01 15:29:58 +0530

    Aliens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I am here to save Earth from <code>Aliens</code>
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
